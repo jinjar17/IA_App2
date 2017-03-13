010e606302ad9d3dbd279775733a00a66c904e4e
Jaron Jin <Jaron Jin@192.168.0.5>
2017-03-12 21:50:42 -0700

    Merge origin/master
    Conflicts:
    	nbproject/project.properties
    	src/ia_app/IA_App.java

--- src/ia_app/IA_App.java
+++ src/ia_app/IA_App.java
@@ -11,24 +11,6 @@ public class IA_App {
         f.setSize(500, 500);
         f.setVisible(true);
     }    
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package ia_app;
 
-/**
- *
- * @author Jaron Jin
- */
-public class IA_App {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
     
 }
